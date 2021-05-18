fa7b6454759a79a7e414ee8a8272af94c5ed8ae8
Viktor <todosijevicviktor998@gmail.com>
2021-05-18 15:04:41 +0200

    Poredio greske za razlicite parametre

--- Procena faze signala/procena_greske_procene.m
+++ Procena faze signala/procena_greske_procene.m
@@ -6,20 +6,30 @@ set(groot,'defaulttextinterpreter','latex');
 set(groot, 'defaultAxesTickLabelInterpreter','latex');  
 set(groot, 'defaultLegendInterpreter','latex');
 %%
-xsize = 1024;
-Fs = 2000;
+
+% Ideje: 
+%   - (tesko je reci, zasto bi ovo radilo?) da se smanji broj odbiraka (2048->1024 npr)
+%   - (Ovo ne valja) da se smanji ucestanost odabiranja sto bi povecalo 
+%   rezoluciju?
+%   - (cini se da jeste mnogo bolje)da se poveca ucestanost odabiranja 
+%   sto bi povecalo vremensku
+%   rezoluciju
+%   - da se usrednjavaju procene u okolnim tackama
+
+xsize = 512;
+Fs = 4000;
 
 t = 1/Fs * (1:xsize);
 
 A = 6000;
 
-N = 2^15;
+N = 2^14;
 
 naxis = 0:N/2;
 faxis1 = naxis/(N/2) * Fs / 2;
 
-freqs = 5:0.01:100;
-phases = -pi:0.1:pi;
+freqs = 20:0.01:100;
+phases = -pi/2:0.1:pi/2;
 
 errors = zeros(length(freqs), length(phases));
 tic
@@ -44,3 +54,13 @@ surf(X,Y,errors')
 title("Greska procene faze")
 xlabel("f [Hz]")
 ylabel("$\phi$ [rad]")
+zlabel("$|$error$|$ [rad]")
+
+
+% filtered_error = imgaussfilt(errors, 10);
+% figure;
+% surf(X,Y,filtered_error')
+% title("Greska procene faze - filtrirano")
+% xlabel("f [Hz]")
+% ylabel("$\phi$ [rad]")
+% zlabel("$|$error$|$ [rad]")
