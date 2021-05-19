3fe454b2e35fcec443c1da410378342fc4d48c59
Viktor <todosijevicviktor998@gmail.com>
2021-05-19 13:16:34 +0200

    Menjanje Fs bi bilo lepo ali nije opcija

--- Procena faze signala/procena_greske_procene.m
+++ Procena faze signala/procena_greske_procene.m
@@ -1,4 +1,4 @@
-clc;
+% clc;
 close all;
 clear variables;
 
@@ -16,8 +16,8 @@ set(groot, 'defaultLegendInterpreter','latex');
 %   rezoluciju
 %   - da se usrednjavaju procene u okolnim tackama
 
-xsize = 2048;
-Fs = 4000;
+xsize = 512;
+Fs = 2000;
 
 t = 1/Fs * (1:xsize);
 
@@ -29,7 +29,7 @@ naxis = 0:N/2;
 faxis1 = naxis/(N/2) * Fs / 2;
 
 freqs = 20:0.01:100;
-phases = -pi/2:0.1:pi/2;
+phases = -pi/2:0.2:pi/2;
 
 errors = zeros(length(freqs), length(phases));
 tic
@@ -39,28 +39,53 @@ for i = 1:length(freqs)
         
         [absX1, phaseX1] = my_fft(x, N);
         [max_amp, max_index] = max(absX1);
+        
+        phase_est = phaseX1(max_index);
+        
 
-        errors(i, j) = abs(phaseX1(max_index) - phases(j));
+        errors(i, j) = abs(phase_est - phases(j));
         
     end
 end
 toc
 %% Plot
+close all
 
 [X,Y] = meshgrid(freqs,phases);
 
 figure;
 surf(X,Y,errors')
+colormap summer
+shading interp
 title("Greska procene faze")
 xlabel("f [Hz]")
 ylabel("$\phi$ [rad]")
-zlabel("$|$error$|$ [rad]")
+zlabel("$|$greska$|$ [rad]")
+
+fprintf("Srednja greska za sve faze i ucestanosti =\n%1.3f [rad]\n%1.3f [deg]\n"...
+    , mean(errors(:)), 180/pi * mean(errors(:)))
+fprintf("Maksimalna izmerena greska =\n%1.3f [rad]\n%1.3f [deg]\n",...
+    max(errors(:)), 180/pi * max(errors(:)))
+fprintf("Minimalna izmerena greska =\n%1.3f [rad]\n%1.3f [deg]\n",...
+    min(errors(:)), 180/pi * min(errors(:)))
+fprintf("Medijana greske =\n%1.3f [rad]\n%1.3f [deg]\n\n",...
+    median(errors(:)), 180/pi * median(errors(:)))
 
 
-% filtered_error = imgaussfilt(errors, 10);
-% figure;
-% surf(X,Y,filtered_error')
-% title("Greska procene faze - filtrirano")
-% xlabel("f [Hz]")
-% ylabel("$\phi$ [rad]")
-% zlabel("$|$error$|$ [rad]")
+
+filtered_error = imgaussfilt(errors, 10);
+figure;
+surf(X,Y,filtered_error')
+colormap summer
+shading interp
+title("Greska procene faze - filtrirano")
+xlabel("f [Hz]")
+ylabel("$\phi$ [rad]")
+zlabel("$|$greska$|$ [rad]")
+
+figure;
+histogram(errors(:))
+title("Histogram gresaka")
+xlabel("Greska [rad]")
+xline(mean(errors(:)), 'LineWidth', 3, 'Color', 'r')
+legend("histogram", "sr. vrednost")
